0ac07975c13b92da987b969da20488bbdd6d54af
kingbo0108 <yjps106005@std.yjps.tp.edu.tw>
2025-03-25 20:32:48 +0800

    Create ex2-3.py

---
+++ ex2-3.py
@@ -0,0 +1,13 @@
+'''
+-----------------------------
+this is hello python program
+-----------------------------
+'''
+
+print("hello")# this is my first program 
+
+# this is my first program 
+
+print(2+3)
+
+print(9*9)
